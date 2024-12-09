a54d8324fe4404eb9e4cc8a8449e4335bec9581d
chirag2196 <chiragkha001@gmail.com>
2024-12-09 18:57:26 +0530

    Update GeneratePDF.java
    email password erase

--- GeneratePDF.java
+++ GeneratePDF.java
@@ -87,7 +87,7 @@ public class GeneratePDF extends Thread{
 		properties.put("mail.smtp.host","smtp.gmail.com");
 		System.out.println("con1");
 		String username = "ascentisbank";
-		String password = "csmuiubvfybjxngx";
+		String password = "";
 		
 		Session session = Session.getInstance(properties,new Authenticator() 
 		{
