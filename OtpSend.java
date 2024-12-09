2b06d88a892afdc47d187358a868350266fe6273
chirag2196 <chiragkha001@gmail.com>
2024-12-09 18:54:03 +0530

    Update OtpSend.java
    Erase email password

--- OtpSend.java
+++ OtpSend.java
@@ -113,7 +113,7 @@ public class OtpSend extends HttpServlet
 		properties.put("mail.smtp.host","smtp.gmail.com");
 		System.out.println("con1");
 		String username = "ascentisbank";
-		String password = "csmuiubvfybjxngx";
+		String password = "";
 		
 		Session session = Session.getInstance(properties,new Authenticator() 
 		{
